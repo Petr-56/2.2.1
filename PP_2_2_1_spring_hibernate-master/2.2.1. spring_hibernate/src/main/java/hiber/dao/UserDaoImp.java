118ee00d3597e402315a4dd1f4d31858f1cc788b
Petr-56 <petkog2156-2@mail.ru>
2025-02-20 16:38:57 +0300

    Update UserDaoImp.java

--- PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,16 +2,11 @@ package hiber.dao;
 
 import hiber.model.Car;
 import hiber.model.User;
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.context.spi.CurrentSessionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import java.util.Currency;
 import java.util.List;
 
 @Repository
@@ -34,19 +29,11 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public User findByCar(String car_model, String car_series) {
-      TypedQuery<Car> findCarQuery = sessionFactory.getCurrentSession().createQuery("from Car where model = :car_model and series = :car_series")
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(
+                      "select u from User u join u.car c where c.model = :car_model and c.series = :car_series", User.class)
               .setParameter("car_model", car_model)
               .setParameter("car_series", car_series);
-      List<Car> findCarList = findCarQuery.getResultList();
-      if (!findCarList.isEmpty()) {
-         Car findCar = findCarList.get(0);
-         List<User> ListUser = listUsers();
-         User FindUser = ListUser.stream()
-                 .filter(user -> user.getCar().equals(findCar))
-                 .findAny()
-                 .orElse(null);
-         return FindUser;
-      }
-      return null;
+      List<User> users = query.getResultList();
+      return users.isEmpty() ? null : users.get(0);
    }
 }
